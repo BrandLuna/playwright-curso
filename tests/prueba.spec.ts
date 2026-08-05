7a48f88fb8c2b9d5ebdfc5d5d2bdd48aadd33df0
Brandon Luna <brandonf.luna@gmail.com>
2026-08-05 16:26:38 -0500

    chore: eliminar archivos viejos del proyecto original

--- tests/prueba.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from './fixtures';
-
-test('checkout flow con POM', async ({ page, loginPage, inventoryPage, cartPage }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await loginPage.login('standard_user', 'secret_sauce');
-  await inventoryPage.addBackpackAndGoToCart();
-  await cartPage.checkout();
-});
